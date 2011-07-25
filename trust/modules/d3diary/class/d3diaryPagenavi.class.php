0e30cc84fffc317de1ef14208d56733e3dc850b9
naao <naao@728962c7-3d4f-4d3d-a24f-1df2ec58a8c8>
2011-07-25 12:02:01 +0000

    Improve pagenavi selected style again
    git-svn-id: https://svn.sourceforge.jp/svnroot/d3diary/d3diary@47 728962c7-3d4f-4d3d-a24f-1df2ec58a8c8

--- trust/modules/d3diary/class/d3diaryPagenavi.class.php
+++ trust/modules/d3diary/class/d3diaryPagenavi.class.php
@@ -87,13 +87,13 @@ class d3diaryPageNav {
             while ( $i <= $total_pages ) {
                 if ( $i == $current_page ) {
                 	$nav[$j]['txt']= $i;
-                	$nav[$j]['class']= "txt";
+                	$nav[$j]['class']= "this";
                 	$nav[$j]['url']= "";
                 	$j++;
                 } elseif ( ($i > $current_page - $offset && $i < $current_page + $offset ) || $i == 1 || $i == $total_pages ) {
                     if ( $i == $total_pages && $current_page < $total_pages - $offset ) {
                 	$nav[$j]['txt']= '... ';
-                	$nav[$j]['class']= "non";
+                	$nav[$j]['class']= "txt";
                 	$nav[$j]['url']= "";
                 	$j++;
                     }
@@ -103,7 +103,7 @@ class d3diaryPageNav {
                 	$j++;
                     if ( $i == 1 && $current_page > 1 + $offset ) {
                 	$nav[$j]['txt']= '... ';
-                	$nav[$j]['class']= "non";
+                	$nav[$j]['class']= "txt";
                 	$nav[$j]['url']= "";
                 	$j++;
                     }
